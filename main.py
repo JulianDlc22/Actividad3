d133ad61638d2ed6b72891d8de3730ffcca95fc9
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-07-07 11:25:36 -0600

    Se agregaron los case

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ def menu():
     print("2. Restar")
     print("3. Salir")
 
-menu()
-opcion = int(input("Ingresa una opcion: "))
+while True:
+    menu()
+    opcion = int(input("Ingresa una opcion: "))
+    match opcion:
+        case 1:
+            numero = int(input("Ingresa el primer numero: "))
+            numero2 = int(input("Ingresa el segundo numero: "))
+            suma = numero + numero2
+            print("El resultado es: ", suma)
+        case 2:
+            numero = int(input("Ingresa el primer numero: "))
+            numero2 = int(input("Ingresa el segundo numero: "))
+            resta = numero - numero2
+            print("El resultado es: ", resta)
+        case 3:
+            print("Gracias por usar el programa")
 
-if opcion == 1:
-    numero = int(input("Ingresa el primer numero: "))
-    numero2 = int(input("Ingresa el segundo numero: "))
-    suma = numero + numero2
-    print("El resultado es: ", suma)
